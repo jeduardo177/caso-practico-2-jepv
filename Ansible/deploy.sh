43c72cfa06476f6c94bd95de9f156b37afbe83d4
Cloud User <jeduardo177@jepv-cp02-smallvm-worker02.u4xi2lco4xcedcdzmdg0ctcpud.ax.internal.cloudapp.net>
2022-03-14 15:48:10 +0100

    ansible 04 playbook

--- Ansible/deploy.sh
+++ Ansible/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#ansible-playbook -i hosts 01-initial-setup.yaml
-#ansible-playbook -i hosts 02-configure-nfs.yaml
+ansible-playbook -i hosts 01-initial-setup.yaml
+ansible-playbook -i hosts 02-configure-nfs.yaml
 ansible-playbook -i hosts 03-configure-k8-common.yaml
+ansible-playbook -i hosts 04-configure-k8-master.yaml
